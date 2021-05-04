1e35324a2b9f28664de8c1d3562ccbd43a7f3017
Henny Davidowitz <hhalpert18@gmail.com>
2021-05-04 02:01:38 -0400

    class 43

--- python/43/app.py
+++ python/43/app.py
@@ -1,3 +1,19 @@
 print("Hello World")
 x = 5
-print(x)
+print(x, type(x))
+x = 'Hello'
+print(x, type(x))
+y = True
+# boolean
+print(y, type(y))
+z = 1, 2
+print(z, type(z))
+# this is a comment
+
+my_long_variable_name = 5
+my_very_long_int = 456345634643345564648656434646486465464646
+print(my_very_long_int, type(my_very_long_int))
+
+print(id(my_very_long_int))
+my_very_long_int += 1
+print(id(my_very_long_int))
